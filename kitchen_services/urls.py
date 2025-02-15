2f74276948b51c5ffb9322fbb093ba3fdacb14a2
Andrii Fedchuk <andriyfedchuk2006@gmail.com>
2025-02-15 10:09:21 +0100

    add cook-Ålist url pattern to urls.py

--- kitchen_services/urls.py
+++ kitchen_services/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
-from kitchen_services.views import index, DishListView, DishTypeListView
+from kitchen_services.views import index, DishListView, DishTypeListView, CookListView
 
 
 urlpatterns = [
     path("", index, name="index"),
     path("dishes/", DishListView.as_view(), name="dish-list"),
     path("dish-types/", DishTypeListView.as_view(), name="dish-type-list"),
+    path("cooks/", CookListView.as_view(), name="cook-list"),
 ]
 
 app_name = "kitchen_services"
