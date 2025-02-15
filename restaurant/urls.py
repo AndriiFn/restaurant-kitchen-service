ca56b2e459f9433356ec6f9890d060dc3bc8abf7
Andrii Fedchuk <andriyfedchuk2006@gmail.com>
2025-02-15 12:51:47 +0100

    add authentication URLs to support login and logout

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("kitchen_services.urls", namespace="kitchen_services")),
+    path("", include("django.contrib.auth.urls")),
 ] + debug_toolbar_urls()
