255d8798f2f828e60d29c1be5e8d50d06b19adb9
Andrii Fedchuk <andriyfedchuk2006@gmail.com>
2025-02-15 15:02:36 +0100

    add DishCreateView to urls.py

--- kitchen_services/urls.py
+++ kitchen_services/urls.py
@@ -2,10 +2,11 @@ from django.urls import path, include
 from kitchen_services.views import (
     index,
     DishListView,
+    DishDetailView,
+    DishCreateView,
     DishTypeListView,
     CookListView,
     CookDetailView,
-    DishDetailView,
 )
 
 
@@ -13,6 +14,7 @@ urlpatterns = [
     path("", index, name="index"),
     path("dishes/", DishListView.as_view(), name="dish-list"),
     path("dishes/<int:pk>/", DishDetailView.as_view(), name="dish-detail"),
+    path("dishes/create/", DishCreateView.as_view(), name="dish-create"),
     path("dish-types/", DishTypeListView.as_view(), name="dish-type-list"),
     path("cooks/", CookListView.as_view(), name="cook-list"),
     path("cooks/<int:pk>/", CookDetailView.as_view(), name="cook-detail"),
