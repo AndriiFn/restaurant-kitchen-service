3afffff77d7a95cd8c675c2ec003a1120e949d5a
Andrii Fedchuk <andriyfedchuk2006@gmail.com>
2025-02-15 09:14:00 +0100

    add new url path dish-types

--- kitchen_services/urls.py
+++ kitchen_services/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
-from kitchen_services.views import index, DishListView
+from kitchen_services.views import index, DishListView, DishTypeListView
 
 
 urlpatterns = [
     path("", index, name="index"),
     path("dishes/", DishListView.as_view(), name="dish-list"),
+    path("dish-types/", DishTypeListView.as_view(), name="dish-type-list"),
 ]
 
 app_name = "kitchen_services"
