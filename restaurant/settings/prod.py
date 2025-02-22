76c25570026a032043cedd66bfb5f3b0501435f3
Andrii Fedchuk <andriyfedchuk2006@gmail.com>
2025-02-22 17:52:41 +0100

    update settings

--- restaurant/settings/prod.py
+++ restaurant/settings/prod.py
@@ -1,6 +1,9 @@
 import os
 from .base import *
 
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
 ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
